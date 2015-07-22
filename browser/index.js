ecb2e30db46e42738cf917e15929e73920b3a6a6
Michael Garvin <gar@andyet.net>
2015-07-22 09:13:27 -0700

    work in browser

---
+++ browser/index.js
@@ -0,0 +1,15 @@
+/**
+ * Browser test code
+ * We use assert here just to throw on errors so we have
+ * at least some assurance that the code's working
+ */
+
+var JsonRpcWs = require('../');
+var browserClient = JsonRpcWs.createClient();
+
+browserClient.expose('info', function info (params, reply) {
+
+    reply(null, 'browser');
+});
+
+window.browserClient = browserClient;
